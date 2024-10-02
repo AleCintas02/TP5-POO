bd41b8b363b5c2b3edf81ac042c257b810261a30
LautaroLarrozagit config --global user.name LautaroLarrozagit config --global user.name LautaroLarroza <lauutarolarroza@gmail.com>
2024-10-02 20:33:38 -0300

    clase triangulo terminada

--- Triangulo.java
+++ Triangulo.java
@@ -1,33 +1,35 @@
-
-/**
- * Write a description of class Triangulo here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Triangulo
+public class Triangulo extends FiguraGeometrica
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    double base;
+    double altura;
 
     /**
      * Constructor for objects of class Triangulo
      */
-    public Triangulo()
-    {
-        // initialise instance variables
-        x = 0;
+    public Triangulo(double p_base, double p_altura){
+        
+        this.setBase(p_base);
+        this.setAltura(p_altura);
+    }
+    
+    private void setBase(double p_base){
+        this.base=p_base;
+    }
+    private void setAltura(double p_altura){
+        this.altura=p_altura;
+    }
+    
+    public double getBase(){
+        return this.base;
+    }
+    public double getAltura(){
+        return this.altura;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public String nombreFigura(){
+        return "Triangulo";
+    }
+    public double superficie(){
+      return (this.getBase() * this.getAltura()) / 2; 
     }
 }
