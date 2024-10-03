7790996e850bb53b9ada786932d076fa017e1b59
AleCintas02 <alecintas003@hotmail.com>
2024-10-02 21:02:09 -0300

    prueba

--- Jardin.java
+++ Jardin.java
@@ -1,21 +1,18 @@
 import java.util.ArrayList;
-
 public class Jardin
 {
     String nombre;
     ArrayList<FiguraGeometrica>figuras;
 
-<<<<<<< HEAD
-        /**
-         
-    Constructor for objects of class Jardin
-       */
-       public Jardin(String p_nombre, ArrayList<FiguraGeometrica> p_figuras)
-      {
-          this.setNombre(p_nombre);
-          this.setFiguras(p_figuras);
-      }
-  
+    /**
+     
+Constructor for objects of class Jardin
+   */
+   public Jardin(String p_nombre, ArrayList<FiguraGeometrica> p_figuras)
+  {
+      this.setNombre(p_nombre);
+      this.setFiguras(p_figuras);
+  }
   public Jardin(String p_nombre)
   {
       this.setNombre(p_nombre);
@@ -25,6 +22,8 @@ public class Jardin
   private void setNombre(String p_nombre){
       this.nombre=p_nombre;
   }
+  
+  //setFiguras
   private void setFiguras(ArrayList<FiguraGeometrica> p_figuras){
       this.figuras=p_figuras;
   }
@@ -37,36 +36,3 @@ public class Jardin
   }
 
 }
-=======
-    /**
-     * Constructor for objects of class Jardin
-     */
-     public Jardin(String p_nombre, ArrayList<FiguraGeometrica> p_figuras)
-    {
-        this.setNombre(p_nombre);
-        this.setFiguras(p_figuras);
-    }
-    public Jardin(String p_nombre)
-    {
-        this.setNombre(p_nombre);
-        this.setFiguras(new ArrayList<FiguraGeometrica>());
-    }
-    
-    private void setNombre(String p_nombre){
-        this.nombre=p_nombre;
-    }
-    private void setFiguras(ArrayList<FiguraGeometrica> p_figuras){
-        this.figuras=p_figuras;
-    }
-    
-    public String getNombre(){
-        return this.nombre;
-    }
-    public ArrayList<FiguraGeometrica> getFiguras(){
-        return this.figuras;
-    }
-    
-    
- 
-}
->>>>>>> 80810bfe297b0bc33a4f0a2b2618fdccd42335a7
